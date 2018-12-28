b1f72106fde3442a0b17cd12fe546f87d9908cd3
Gennadiy <g.abashkin@petrovich.ru>
2018-12-28 15:08:13 +0300

    Task 2 in proogress, 7 from 9 tests ready

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,39 @@
-export { default } from './Chat';
+import React from 'react';
+import Message from '../Message';
+
+
+class Chat extends React.Component {
+    state = {
+        messages: [],
+        messageInput: '',
+    }
+
+    changeInputMessage = event => {
+        this.setState({messageInput: event.target.value})
+    }
+    
+
+    sendMessageOnEnter = event => {
+        if (event.key === 'Enter') {
+            let newArr = this.state.messages;
+            newArr.push({text: event.target.value});
+            this.setState({
+                messages: newArr,
+                messageInput: '',
+            })
+            console.log(this.state);
+            setTimeout(() => {console.log(this.state)}, 3000)
+        }
+    }
+    
+    render () {
+        return (
+            <div className="chat">
+                <input type="text" className="input-message" onChange={this.changeInputMessage} value={this.state.messageInput} onKeyPress={this.sendMessageOnEnter}/>
+                <Message/>
+            </div>
+        )    
+    }
+}
+
+export default Chat;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,13 @@
-export { default } from './Message';
+import React from 'react';
+import { link } from 'fs';
+
+class Message extends React.Component {
+    render () {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Message;
